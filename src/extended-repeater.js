295531ded7726f8f5d1db58b8588fb80cf045fbd
Anton <antonpomogaev@gmail.com>
2022-03-26 21:09:11 +0300

    Refactor: improve readability in extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,61 +2,64 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(str, {
-  repeatTimes = 0,
-  separator = '+',
-  addition = '',
-  additionRepeatTimes = 0,
-  additionSeparator= '|', 
-}) {
+function repeater(
+  str,
+  {
+    repeatTimes = 0,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 0,
+    additionSeparator = '|',
+  }
+) {
   // Check types of primary arguments
   if (typeof str !== 'string') str = String(str);
-  if (typeof addition  !== 'string') addition = String(addition);
+  if (typeof addition !== 'string') addition = String(addition);
 
-  // Get full addition string 
-  let combinedAddition = '';
-  if (additionRepeatTimes > 0) {
-    for (let i = 0; i < additionRepeatTimes; i++) {
-      combinedAddition += addition;
-      if (i !== additionRepeatTimes - 1) {
-        combinedAddition += additionSeparator;
-      }
-    }
-  } else {
-    combinedAddition = addition;
-  }
+  // Get full addition string
+  let combinedAddition = getFullString(
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  );
 
   // Get full main string
   str += combinedAddition;
-  let combinedStr = '';
+  str = getFullString(str, repeatTimes, separator);
 
-  if (repeatTimes > 0) {
-    for (let i = 0; i < repeatTimes; i++) {
-      combinedStr += str;
-      if (i !== repeatTimes - 1) {
-        combinedStr += separator;
+  return str;
+}
+
+function getFullString(base, repeats, separator) {
+  let combined = '';
+
+  if (repeats > 0) {
+    for (let i = 0; i < repeats; i++) {
+      combined += base;
+      if (i !== repeats - 1) {
+        combined += separator;
       }
     }
   } else {
-    combinedStr = str;
+    combined = base;
   }
 
-  return combinedStr;
+  return combined;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
