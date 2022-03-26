7df03259f0c6a1f16858be49b2e3c342f129b253
Anton <antonpomogaev@gmail.com>
2022-03-26 20:58:42 +0300

    Feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 0,
+  separator = '+',
+  addition = '',
+  additionRepeatTimes = 0,
+  additionSeparator= '|', 
+}) {
+  // Check types of primary arguments
+  if (typeof str !== 'string') str = String(str);
+  if (typeof addition  !== 'string') addition = String(addition);
+
+  // Get full addition string 
+  let combinedAddition = '';
+  if (additionRepeatTimes > 0) {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      combinedAddition += addition;
+      if (i !== additionRepeatTimes - 1) {
+        combinedAddition += additionSeparator;
+      }
+    }
+  } else {
+    combinedAddition = addition;
+  }
+
+  // Get full main string
+  str += combinedAddition;
+  let combinedStr = '';
+
+  if (repeatTimes > 0) {
+    for (let i = 0; i < repeatTimes; i++) {
+      combinedStr += str;
+      if (i !== repeatTimes - 1) {
+        combinedStr += separator;
+      }
+    }
+  } else {
+    combinedStr = str;
+  }
+
+  return combinedStr;
 }
 
 module.exports = {
